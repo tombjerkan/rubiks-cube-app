3df94e67e6d127d65120e3820a0e1fa9a73eddc5
Thomas Bjerkan <t.bjerkan@gmail.com>
2017-11-16 13:17:26 +0100

    Remove title bar from app

--- app/src/main/java/com/bjerkan/rubikscubeapp/ScanCubeActivity.java
+++ app/src/main/java/com/bjerkan/rubikscubeapp/ScanCubeActivity.java
@@ -1,9 +1,9 @@
 package com.bjerkan.rubikscubeapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class ScanCubeActivity extends AppCompatActivity {
+public class ScanCubeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
