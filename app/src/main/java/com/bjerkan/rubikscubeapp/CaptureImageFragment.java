1d4b9e96f0a990d746a941c9a36c6b1a43e708d2
Thomas Bjerkan <t.bjerkan@gmail.com>
2017-11-16 14:22:25 +0100

    Use container for fragment in activity

--- app/src/main/java/com/bjerkan/rubikscubeapp/CaptureImageFragment.java
+++ app/src/main/java/com/bjerkan/rubikscubeapp/CaptureImageFragment.java
@@ -1,6 +1,6 @@
 package com.bjerkan.rubikscubeapp;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
--- app/src/main/java/com/bjerkan/rubikscubeapp/ScanCubeActivity.java
+++ app/src/main/java/com/bjerkan/rubikscubeapp/ScanCubeActivity.java
@@ -1,17 +1,25 @@
 package com.bjerkan.rubikscubeapp;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import org.opencv.core.Mat;
 
-public class ScanCubeActivity extends Activity
+public class ScanCubeActivity extends FragmentActivity
         implements CaptureImageFragment.OnImageCapturedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_cube);
+
+        if (savedInstanceState == null) {
+            CaptureImageFragment fragment = new CaptureImageFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container, fragment)
+                    .commit();
+        }
     }
 
     @Override
