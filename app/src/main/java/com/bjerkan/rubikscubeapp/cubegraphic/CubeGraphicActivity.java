ce51f3a3030eb1dcf9957fdc035bffc60481fef6
Thomas Bjerkan <t.bjerkan@gmail.com>
2017-11-20 20:03:37 +0100

    Remove now unused button onClick methods

--- app/src/main/java/com/bjerkan/rubikscubeapp/cubegraphic/CubeGraphicActivity.java
+++ app/src/main/java/com/bjerkan/rubikscubeapp/cubegraphic/CubeGraphicActivity.java
@@ -56,54 +56,6 @@ public class CubeGraphicActivity extends Activity implements AnimationFinishedLi
                 .collect(Collectors.toList());
     }
 
-    public void front(View view) {
-        rubiksCubeModel.front();
-    }
-
-    public void frontInv(View view) {
-        rubiksCubeModel.frontInv();
-    }
-
-    public void left(View view) {
-        rubiksCubeModel.left();
-    }
-
-    public void leftInv(View view) {
-        rubiksCubeModel.leftInv();
-    }
-
-    public void right(View view) {
-        rubiksCubeModel.right();
-    }
-
-    public void rightInv(View view) {
-        rubiksCubeModel.rightInv();
-    }
-
-    public void top(View view) {
-        rubiksCubeModel.top();
-    }
-
-    public void topInv(View view) {
-        rubiksCubeModel.topInv();
-    }
-
-    public void bottom(View view) {
-        rubiksCubeModel.bottom();
-    }
-
-    public void bottomInv(View view) {
-        rubiksCubeModel.bottomInv();
-    }
-
-    public void rotate(View view) {
-        rubiksCubeModel.rotate();
-    }
-
-    public void rotateInv(View view) {
-        rubiksCubeModel.rotateInv();
-    }
-
     @Override
     public void animationFinished() {
         if (nextActionIndex >= solvingActions.size()) {
