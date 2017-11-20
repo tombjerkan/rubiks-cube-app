3ac2b1d88e5216dd9a47e38b6baffde8c4268816
Thomas Bjerkan <t.bjerkan@gmail.com>
2017-11-20 12:16:08 +0100

    Solve bottom cube edges

--- app/src/main/java/com/bjerkan/rubikscubeapp/rubikscube/Solver.java
+++ app/src/main/java/com/bjerkan/rubikscubeapp/rubikscube/Solver.java
@@ -1,5 +1,10 @@
 package com.bjerkan.rubikscubeapp.rubikscube;
 
+import com.bjerkan.rubikscubeapp.CubeScanner.RubiksColour;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Solver {
     public static void solve(RubiksCube cube) {
         solveBottomEdges(cube);
@@ -10,7 +15,58 @@ public class Solver {
     }
 
     public static void solveBottomEdges(RubiksCube cube) {
+        while (!bottomEdgesOnTop(cube)) {
+            if (cube.bottomFace().topMiddle() == cube.bottomFace().middle()) {
+                while (cube.topFace().bottomMiddle() == cube.bottomFace().middle()) {
+                    cube.top();
+                }
 
+                cube.front().front();
+            } else if (cube.leftFace().middleRight() == cube.bottomFace().middle()) {
+                while (cube.topFace().bottomMiddle() == cube.bottomFace().middle()) {
+                    cube.top();
+                }
+
+                cube.front();
+            } else if (cube.frontFace().middleLeft() == cube.bottomFace().middle()) {
+                while (cube.topFace().middleLeft() == cube.bottomFace().middle()) {
+                    cube.top();
+                }
+
+                cube.leftInv();
+            } else if (cube.frontFace().topMiddle() == cube.bottomFace().middle()) {
+                cube.front().topInv().right();
+            } else if (cube.frontFace().bottomMiddle() == cube.bottomFace().middle()) {
+                while (cube.topFace().bottomMiddle() == cube.bottomFace().middle()) {
+                    cube.top();
+                }
+
+                cube.front().topInv().right();
+            } else {
+                cube.rotate();
+            }
+        }
+
+        for (int face = 0; face < 4; face++) {
+            while (!frontTopEdgeAbovePosition(cube)) {
+                cube.top();
+            }
+
+            cube.front().front().rotate();
+        }
+    }
+
+    private static boolean bottomEdgesOnTop(RubiksCube cube) {
+        return allAreColour(
+                cube.bottomFace().middle(),
+                Arrays.asList(
+                        cube.topFace().topMiddle(), cube.topFace().middleLeft(),
+                        cube.topFace().middleRight(), cube.topFace().bottomMiddle()));
+    }
+
+    private static boolean frontTopEdgeAbovePosition(RubiksCube cube) {
+        return cube.frontFace().topMiddle() == cube.frontFace().middle() &&
+                cube.topFace().bottomMiddle() == cube.bottomFace().middle();
     }
 
     public static void solveBottomCorners(RubiksCube cube) {
@@ -27,5 +83,9 @@ public class Solver {
 
     public static void solveTopCorners(RubiksCube cube) {
 
+    }
+
+    private static boolean allAreColour(RubiksColour matchColour, List<RubiksColour> colours) {
+        return colours.stream().allMatch(colour -> colour == matchColour);
     }
 }
