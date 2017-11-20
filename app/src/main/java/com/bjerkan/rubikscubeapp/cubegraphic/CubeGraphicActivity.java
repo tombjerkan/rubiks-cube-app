4d732ef49d7311c0d1aca2da032450cb2d6331a6
Thomas Bjerkan <t.bjerkan@gmail.com>
2017-11-20 20:00:45 +0100

    Animate cube model with actions required to solve the cube

--- app/src/main/java/com/bjerkan/rubikscubeapp/cubegraphic/CubeGraphicActivity.java
+++ app/src/main/java/com/bjerkan/rubikscubeapp/cubegraphic/CubeGraphicActivity.java
@@ -6,25 +6,45 @@ import android.view.View;
 
 import com.bjerkan.rubikscubeapp.CubeScanner;
 import com.bjerkan.rubikscubeapp.R;
+import com.bjerkan.rubikscubeapp.rubikscube.RubiksCube;
+import com.bjerkan.rubikscubeapp.rubikscube.Solver;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class CubeGraphicActivity extends Activity {
+import static com.bjerkan.rubikscubeapp.cubegraphic.RubiksCubeModel.AnimationFinishedListener;
+
+public class CubeGraphicActivity extends Activity implements AnimationFinishedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cube_graphic);
 
-        rubiksCubeModel = new RubiksCubeModel(
-                asColourList(getIntent().getStringArrayListExtra(FRONT_COLOURS_ARGUMENT)),
-                asColourList(getIntent().getStringArrayListExtra(LEFT_COLOURS_ARGUMENT)),
-                asColourList(getIntent().getStringArrayListExtra(BACK_COLOURS_ARGUMENT)),
-                asColourList(getIntent().getStringArrayListExtra(RIGHT_COLOURS_ARGUMENT)),
-                asColourList(getIntent().getStringArrayListExtra(TOP_COLOURS_ARGUMENT)),
-                asColourList(getIntent().getStringArrayListExtra(BOTTOM_COLOURS_ARGUMENT))
-        );
+        List<CubeScanner.RubiksColour> frontColours = asColourList(
+                getIntent().getStringArrayListExtra(FRONT_COLOURS_ARGUMENT));
+        List<CubeScanner.RubiksColour> leftColours = asColourList(
+                getIntent().getStringArrayListExtra(LEFT_COLOURS_ARGUMENT));
+        List<CubeScanner.RubiksColour> backColours = asColourList(
+                getIntent().getStringArrayListExtra(BACK_COLOURS_ARGUMENT));
+        List<CubeScanner.RubiksColour> rightColours = asColourList(
+                getIntent().getStringArrayListExtra(RIGHT_COLOURS_ARGUMENT));
+        List<CubeScanner.RubiksColour> topColours = asColourList(
+                getIntent().getStringArrayListExtra(TOP_COLOURS_ARGUMENT));
+        List<CubeScanner.RubiksColour> bottomColours = asColourList(
+                getIntent().getStringArrayListExtra(BOTTOM_COLOURS_ARGUMENT));
+
+        rubiksCubeModel = new RubiksCubeModel(frontColours, leftColours, backColours, rightColours,
+                topColours, bottomColours);
+        rubiksCubeModel.setAnimationFinishedListener(this);
+
+        RubiksCube cube = new RubiksCube(frontColours, leftColours, backColours, rightColours,
+                topColours, bottomColours);
+
+        solvingActions = Solver.solve(cube);
+        nextActionIndex = 0;
+        // Make animations begin as logic to start new animation is in method for animation ending
+        animationFinished();
 
         CubeSurfaceView cubeSurfaceView = findViewById(R.id.cubeSurfaceView);
         cubeSurfaceView.setModel(rubiksCubeModel);
@@ -84,6 +104,55 @@ public class CubeGraphicActivity extends Activity {
         rubiksCubeModel.rotateInv();
     }
 
+    @Override
+    public void animationFinished() {
+        if (nextActionIndex >= solvingActions.size()) {
+            return;
+        }
+
+        RubiksCube.Action nextAction = solvingActions.get(nextActionIndex);
+        nextActionIndex++;
+
+        switch (nextAction) {
+            case FRONT:
+                rubiksCubeModel.front();
+                break;
+            case FRONT_INV:
+                rubiksCubeModel.frontInv();
+                break;
+            case LEFT:
+                rubiksCubeModel.left();
+                break;
+            case LEFT_INV:
+                rubiksCubeModel.leftInv();
+                break;
+            case RIGHT:
+                rubiksCubeModel.right();
+                break;
+            case RIGHT_INV:
+                rubiksCubeModel.rightInv();
+                break;
+            case TOP:
+                rubiksCubeModel.top();
+                break;
+            case TOP_INV:
+                rubiksCubeModel.topInv();
+                break;
+            case BOTTOM:
+                rubiksCubeModel.bottom();
+                break;
+            case BOTTOM_INV:
+                rubiksCubeModel.bottomInv();
+                break;
+            case ROTATE:
+                rubiksCubeModel.rotate();
+                break;
+            case ROTATE_INV:
+                rubiksCubeModel.rotateInv();
+                break;
+        }
+    }
+
     public static final String FRONT_COLOURS_ARGUMENT = "com.bjerkan.FRONT_COLOURS_ARGUMENT";
     public static final String LEFT_COLOURS_ARGUMENT = "com.bjerkan.LEFT_COLOURS_ARGUMENT";
     public static final String BACK_COLOURS_ARGUMENT = "com.bjerkan.BACK_COLOURS_ARGUMENT";
@@ -92,4 +161,7 @@ public class CubeGraphicActivity extends Activity {
     public static final String BOTTOM_COLOURS_ARGUMENT = "com.bjerkan.BOTTOM_COLOURS_ARGUMENT";
 
     private RubiksCubeModel rubiksCubeModel;
+
+    private List<RubiksCube.Action> solvingActions;
+    private int nextActionIndex;
 }
