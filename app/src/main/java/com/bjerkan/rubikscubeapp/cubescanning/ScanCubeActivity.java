5188495da5dfb258e8ec7b2a11070a04b7a3a2e4
Thomas Bjerkan <t.bjerkan@gmail.com>
2017-11-21 16:49:38 +0100

    Remove no longer needed import statement

--- app/src/main/java/com/bjerkan/rubikscubeapp/cubescanning/ScanCubeActivity.java
+++ app/src/main/java/com/bjerkan/rubikscubeapp/cubescanning/ScanCubeActivity.java
@@ -5,7 +5,6 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
 import android.widget.Toast;
 
 import com.bjerkan.rubikscubeapp.R;
