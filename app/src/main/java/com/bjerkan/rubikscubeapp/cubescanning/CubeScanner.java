a5954d261ab1fc7bec9c30cb990ba0f22fa02ec0
Thomas Bjerkan <t.bjerkan@gmail.com>
2017-11-21 17:21:11 +0100

    Move Line class to separate file

--- app/src/main/java/com/bjerkan/rubikscubeapp/cubescanning/CubeScanner.java
+++ app/src/main/java/com/bjerkan/rubikscubeapp/cubescanning/CubeScanner.java
@@ -359,66 +359,6 @@ public class CubeScanner {
         }
     }
 
-    private class Line {
-        Line(double rho, double theta) {
-            // Ensure similar lines have numerically close values
-            if (rho >= 0) {
-                this.rho = rho;
-                this.theta = theta;
-            } else {
-                this.rho = -rho;
-                this.theta = theta - Math.PI;
-            }
-        }
-
-        double rho() {
-            return rho;
-        }
-
-        double theta() {
-            return theta;
-        }
-
-        boolean isHorizontal() {
-            return (theta > (Math.PI / 2) - ORTHOGONAL_THRESHOLD) &&
-                    (theta < (Math.PI / 2) + ORTHOGONAL_THRESHOLD);
-        }
-
-        boolean isVertical() {
-            return (theta > -ORTHOGONAL_THRESHOLD) && (theta < ORTHOGONAL_THRESHOLD);
-        }
-
-        boolean isOrthogonal() {
-            return isHorizontal() || isVertical();
-        }
-
-        boolean isSimilar(Line otherLine) {
-            return Math.abs(rho - otherLine.rho) < SIMILARITY_RHO_THRESHOLD &&
-                    Math.abs(theta - otherLine.theta) < SIMILARITY_THETA_THRESHOLD;
-        }
-
-        Point intersection(Line otherLine) {
-            double cosTheta = Math.cos(theta);
-            double sinTheta = Math.sin(theta);
-            double otherCosTheta = Math.cos(otherLine.theta);
-            double otherSinTheta = Math.sin(otherLine.theta);
-
-            double det = cosTheta * otherSinTheta - sinTheta * otherCosTheta;
-
-            if (det == 0) {
-                return null;
-            }
-
-            double x = (otherSinTheta * rho - sinTheta * otherLine.rho) / det;
-            double y = (cosTheta * otherLine.rho - otherCosTheta * rho) / det;
-
-            return new Point(x, y);
-        }
-
-        private final double rho;
-        private final double theta;
-    }
-
     private boolean successful;
 
     private Mat originalImage;
@@ -440,7 +380,4 @@ public class CubeScanner {
     private static final int CANNY_THRESHOLD_1 = 10;
     private static final int CANNY_THRESHOLD_2 = 25;
     private static final int HOUGH_THRESHOLD = 100;
-    private static final double ORTHOGONAL_THRESHOLD = Math.PI/36;
-    private static final double SIMILARITY_RHO_THRESHOLD = 100;
-    private static final double SIMILARITY_THETA_THRESHOLD = Math.PI / 18;
 }
---
+++ app/src/main/java/com/bjerkan/rubikscubeapp/cubescanning/Line.java
@@ -0,0 +1,67 @@
+package com.bjerkan.rubikscubeapp.cubescanning;
+
+import org.opencv.core.Point;
+
+class Line {
+    Line(double rho, double theta) {
+        // Ensure similar lines have numerically close values
+        if (rho >= 0) {
+            this.rho = rho;
+            this.theta = theta;
+        } else {
+            this.rho = -rho;
+            this.theta = theta - Math.PI;
+        }
+    }
+
+    double rho() {
+        return rho;
+    }
+
+    double theta() {
+        return theta;
+    }
+
+    boolean isHorizontal() {
+        return (theta > (Math.PI / 2) - ORTHOGONAL_THRESHOLD) &&
+                (theta < (Math.PI / 2) + ORTHOGONAL_THRESHOLD);
+    }
+
+    boolean isVertical() {
+        return (theta > -ORTHOGONAL_THRESHOLD) && (theta < ORTHOGONAL_THRESHOLD);
+    }
+
+    boolean isOrthogonal() {
+        return isHorizontal() || isVertical();
+    }
+
+    boolean isSimilar(Line otherLine) {
+        return Math.abs(rho - otherLine.rho) < SIMILARITY_RHO_THRESHOLD &&
+                Math.abs(theta - otherLine.theta) < SIMILARITY_THETA_THRESHOLD;
+    }
+
+    Point intersection(Line otherLine) {
+        double cosTheta = Math.cos(theta);
+        double sinTheta = Math.sin(theta);
+        double otherCosTheta = Math.cos(otherLine.theta);
+        double otherSinTheta = Math.sin(otherLine.theta);
+
+        double det = cosTheta * otherSinTheta - sinTheta * otherCosTheta;
+
+        if (det == 0) {
+            return null;
+        }
+
+        double x = (otherSinTheta * rho - sinTheta * otherLine.rho) / det;
+        double y = (cosTheta * otherLine.rho - otherCosTheta * rho) / det;
+
+        return new Point(x, y);
+    }
+
+    private final double rho;
+    private final double theta;
+
+    private static final double ORTHOGONAL_THRESHOLD = Math.PI/36;
+    private static final double SIMILARITY_RHO_THRESHOLD = 100;
+    private static final double SIMILARITY_THETA_THRESHOLD = Math.PI / 18;
+}
