11f602732a034e5b0387a26ab325d66eecc34b96
Thomas Bjerkan <t.bjerkan@gmail.com>
2017-11-24 12:02:38 +0100

    Add doc comments for the replace* methods

--- app/src/main/java/com/bjerkan/rubikscubeapp/rubikscube/RubiksCubeFace.java
+++ app/src/main/java/com/bjerkan/rubikscubeapp/rubikscube/RubiksCubeFace.java
@@ -84,6 +84,14 @@ class RubiksCubeFace {
         return rotated().rotated().rotated();
     }
 
+    /**
+     * Returns a new RubiksCubeFace of this face with the top row colours replaced.
+     *
+     * @param newTopLeft new colour for top-left square
+     * @param newTopMiddle new colour for top-middle square
+     * @param newTopRight new colour for top-right square
+     * @return a new RubiksCubeFace of this face with the top row colours replaced
+     */
     RubiksCubeFace replaceTopRow(
             Colour newTopLeft, Colour newTopMiddle, Colour newTopRight) {
         return new RubiksCubeFace(
@@ -92,10 +100,24 @@ class RubiksCubeFace {
                 bottomLeft, bottomMiddle, bottomRight);
     }
 
+    /**
+     * Returns a new RubiksCubeFace of this face with the top row colours replaced.
+     *
+     * @param colours list of new colours for the top row in the order left, middle, right
+     * @return a new RubiksCubeFace of this face with the top row colours replaced
+     */
     RubiksCubeFace replaceTopRow(List<Colour> colours) {
         return replaceTopRow(colours.get(0), colours.get(1), colours.get(2));
     }
 
+    /**
+     * Returns a new RubiksCubeFace of this face with the bottom row colours replaced.
+     *
+     * @param newBottomLeft new colour for bottom-left square
+     * @param newBottomMiddle new colour for bottom-middle square
+     * @param newBottomRight new colour for bottom-right square
+     * @return a new RubiksCubeFace of this face with the bottom row colours replaced
+     */
     RubiksCubeFace replaceBottomRow(
             Colour newBottomLeft, Colour newBottomMiddle, Colour newBottomRight) {
         return new RubiksCubeFace(
@@ -104,10 +126,24 @@ class RubiksCubeFace {
                 newBottomLeft, newBottomMiddle, newBottomRight);
     }
 
+    /**
+     * Returns a new RubiksCubeFace of this face with the bottom row colours replaced.
+     *
+     * @param colours list of new colours for the bottom row in the order left, middle, right
+     * @return a new RubiksCubeFace of this face with the bottom row colours replaced
+     */
     RubiksCubeFace replaceBottomRow(List<Colour> colours) {
         return replaceBottomRow(colours.get(0), colours.get(1), colours.get(2));
     }
 
+    /**
+     * Returns a new RubiksCubeFace of this face with the left column colours replaced.
+     *
+     * @param newTopLeft new colour for top-left square
+     * @param newMiddleLeft new colour for middle-left square
+     * @param newBottomLeft new colour for bottom-left square
+     * @return a new RubiksCubeFace of this face with the left column colours replaced
+     */
     RubiksCubeFace replaceLeftColumn(
             Colour newTopLeft, Colour newMiddleLeft, Colour newBottomLeft) {
         return new RubiksCubeFace(
@@ -116,10 +152,24 @@ class RubiksCubeFace {
                 newBottomLeft, bottomMiddle, bottomRight);
     }
 
+    /**
+     * Returns a new RubiksCubeFace of this face with the left column colours replaced.
+     *
+     * @param colours list of new colours for the left column in the order top, middle, bottom
+     * @return a new RubiksCubeFace of this face with the left column colours replaced
+     */
     RubiksCubeFace replaceLeftColumn(List<Colour> colours) {
         return replaceLeftColumn(colours.get(0), colours.get(1), colours.get(2));
     }
 
+    /**
+     * Returns a new RubiksCubeFace of this face with the right column colours replaced.
+     *
+     * @param newTopRight new colour for top-right square
+     * @param newMiddleRight new colour for middle-right square
+     * @param newBottomRight new colour for bottom-right square
+     * @return a new RubiksCubeFace of this face with the right column colours replaced
+     */
     RubiksCubeFace replaceRightColumn(
             Colour newTopRight, Colour newMiddleRight, Colour newBottomRight) {
         return new RubiksCubeFace(
@@ -128,6 +178,12 @@ class RubiksCubeFace {
                 bottomLeft, bottomMiddle, newBottomRight);
     }
 
+    /**
+     * Returns a new RubiksCubeFace of this face with the right column colours replaced.
+     *
+     * @param colours list of new colours for the right column in the order top, middle, bottom
+     * @return a new RubiksCubeFace of this face with the right column colours replaced
+     */
     RubiksCubeFace replaceRightColumn(List<Colour> colours) {
         return replaceRightColumn(colours.get(0), colours.get(1), colours.get(2));
     }
