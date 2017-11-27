b17e358c2cb8bfa549600c3d14d44d97afee289d
Thomas Bjerkan <t.bjerkan@gmail.com>
2017-11-27 09:58:49 +0100

    Add doc comments for CubeSurfaceView

--- app/src/main/java/com/bjerkan/rubikscubeapp/cubegraphic/CubeSurfaceView.java
+++ app/src/main/java/com/bjerkan/rubikscubeapp/cubegraphic/CubeSurfaceView.java
@@ -4,10 +4,18 @@ import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.util.AttributeSet;
 
+/**
+ * A view to which a Rubik's Cube graphic can be drawn.
+ */
 class CubeSurfaceView extends GLSurfaceView {
 
     private final CubeRenderer cubeRenderer;
 
+    /**
+     * Standard view constructor. Displays nothing initially until a model is set using setModel.
+     *
+     * @param context the context to run the view in
+     */
     public CubeSurfaceView(Context context){
         super(context);
 
@@ -18,7 +26,12 @@ class CubeSurfaceView extends GLSurfaceView {
         setRenderer(cubeRenderer);
     }
 
-    // Needed to use from layout file
+    /**
+     * Standard view constructor. Displays nothing initially until a model is set using setModel.
+     *
+     * @param context the context to run the view in
+     * @param attrs the XML attributes given to the view
+     */
     public CubeSurfaceView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
@@ -29,6 +42,11 @@ class CubeSurfaceView extends GLSurfaceView {
         setRenderer(cubeRenderer);
     }
 
+    /**
+     * Sets the Rubik's Cube model to draw to the view.
+     *
+     * @param model the model to draw
+     */
     public void setModel(RubiksCubeModel model) {
         cubeRenderer.setModel(model);
     }
